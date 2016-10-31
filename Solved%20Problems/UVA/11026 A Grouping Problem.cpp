5d8a0cbe326a21a715d69d83e430c3931b4a5e2c
mahbub <mahbub.kuet@gmail.com>
2016-10-31 12:12:04 +0600

    4 UVA solution added. 1 UVA solution modified

---
+++ Solved%20Problems/UVA/11026 A Grouping Problem.cpp
@@ -0,0 +1,120 @@
+/*
+    Solved By : Kazi Mahbubur Rahman (MASUD)
+                Software Engineer,
+                Samsung R&D Institute Bangladesh (SRBD),
+                Dhaka, Bangladesh.
+    Time :		0.010
+    Rank :		6
+    Complexity:
+*/
+
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <cctype>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define FOR(i, L, U) for(int i=(int)L; i<=(int)U; i++)
+#define FORD(i, U, L) for(int i=(int)U; i>=(int)L; i--)
+
+#define READ(x) freopen(x, "r", stdin)
+#define WRITE(x) freopen(x, "w", stdout)
+
+#define PQ priority_queue
+#define PB push_back
+#define SZ size()
+
+#define EPS 1e-9
+#define SQR(x) ((x)*(x))
+#define INF 99999999
+#define TO_DEG 57.29577951
+#define PI 2*acos(0.0)
+
+#define ALL_BITS ((1 << 31) - 1)
+#define NEG_BITS(mask) (mask ^= ALL_BITS)
+#define TEST_BIT(mask, i) (mask & (1 << i))
+#define ON_BIT(mask, i) (mask |= (1 << i))
+#define OFF_BIT(mask, i) (mask &= NEG_BITS(1 << i))
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<vector<int> > VVI;
+typedef vector<string> VS;
+typedef vector<bool> VB;
+typedef vector<char> VC;
+typedef vector< vector<bool> > VVB;
+typedef pair<int, int> PII;
+typedef map<int, int> MII;
+typedef map<char, int> MCI;
+typedef map<string, int> MSI;
+
+int GCD(int a,int b){   while(b)b^=a^=b^=a%=b;  return a;   }
+
+// UP, RIGHT, DOWN, LEFT, UPPER-RIGHT, LOWER-RIGHT, LOWER-LEFT, UPPER-LEFT
+int dx[8] = {-1, 0, 1, 0, -1, 1,  1, -1};
+int dy[8] = { 0, 1, 0,-1,  1, 1, -1, -1};
+
+// Represents all moves of a knight in a chessboard
+int dxKnightMove[8] = {-1, -2, -2, -1,  1,  2, 2, 1};
+int dyKnightMove[8] = { 2,  1, -1, -2, -2, -1, 1, 2};
+
+inline int src() { int ret; scanf("%d", &ret); return ret; }
+
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
+
+#define MAX 1000
+
+long long DP[MAX+1][MAX+1];
+
+int main()
+{
+    READ("input.txt");
+    //WRITE("output.txt");
+    int i, j, k;
+    int TC, tc;
+    int N, M;
+
+    double cl = clock();
+    cl = clock() - cl;
+
+    while(scanf("%d %d", &N, &M) == 2) {
+        if(N == 0 && M == 0) break;
+
+        DP[0][0] = 1;
+        FOR(i, 1, N) {
+            int x = src();
+
+            DP[i][0] = 1;
+            FOR(j, 1, i) {
+                DP[i][j] = DP[i-1][j] + DP[i-1][j-1] * x;
+                if(DP[i][j] >= M) DP[i][j] %= M;
+            }
+        }
+
+        long long ans = 0;
+        FOR(i, 1, N) ans = max(ans, DP[N][i]);
+
+        printf("%lld\n", ans);
+
+        fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
+
+    }
+    return 0;
+}
+
