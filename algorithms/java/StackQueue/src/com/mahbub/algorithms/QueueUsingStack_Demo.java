bf8b20cd875f6db1e96a127fc8924ca6e1280f05
Kazi Mahbubur Rahman <mahbub.kuet@gmail.com>
2018-10-28 22:30:32 +0900

    Stack using Queue and Queue using Stack implemented
    Signed-off-by: Kazi Mahbubur Rahman <mahbub.kuet@gmail.com>

--- algorithms/java/StackQueue/src/com/mahbub/algorithms/QueueUsingStack_Demo.java
+++ algorithms/java/StackQueue/src/com/mahbub/algorithms/QueueUsingStack_Demo.java
@@ -1,6 +1,81 @@
 package com.mahbub.algorithms;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 // Ref: http://www.geeksforgeeks.org/?p=5009
 public class QueueUsingStack_Demo {
 
+    public static void main(String[] args) {
+        QueueUsingStack q = new QueueUsingStack();
+        q.push(1);
+        q.push(2);
+        q.push(3);
+
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+        q.push(4);
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+
+        QueueUsingStack q2 = new QueueUsingStack();
+        q2.push(1);
+        q2.push(2);
+        q2.push(3);
+
+        System.out.println(q2.pop());
+        System.out.println(q2.pop());
+        q2.push(4);
+        System.out.println(q2.pop());
+        System.out.println(q2.pop());
+    }
+
+    static class QueueUsingStack {
+        Stack<Integer> s1, s2;
+
+        public QueueUsingStack() {
+            s1 = new Stack<>();
+            s2 = new Stack<>();
+        }
+
+        // Complexity: O(1)
+        public void push(Integer x) {
+            s1.push(x);
+        }
+
+        // Complexity: O(n)
+        public Integer pop() {
+            if (s1.empty()) return null;
+
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+            }
+
+            Integer ret = s2.pop();
+
+            while (!s2.empty()) {
+                s1.push(s2.pop());
+            }
+
+            return ret;
+        }
+
+        // Complexity: O(n)
+        public void push2(Integer x) {
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+            }
+
+            s1.push(x);
+
+            while (!s2.empty()) {
+                s1.push(s2.pop());
+            }
+        }
+
+        // Complexity: O(1)
+        public Integer pop2() {
+            return s1.pop();
+        }
+    }
 }
--- algorithms/java/StackQueue/src/com/mahbub/algorithms/StackUsingQueue_Demo.java
+++ algorithms/java/StackQueue/src/com/mahbub/algorithms/StackUsingQueue_Demo.java
@@ -1,6 +1,58 @@
 package com.mahbub.algorithms;
 
-// Ref: http://www.geeksforgeeks.org/implement-stack-using-queue/
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class StackUsingQueue_Demo {
 
+    public static void main(String[] args) {
+        StackUsingQueue s = new StackUsingQueue();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        s.push(4);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+
+    static class StackUsingQueue {
+        Queue<Integer> q1, q2;
+
+        public StackUsingQueue() {
+            q1 = new LinkedList<>();
+            q2 = new LinkedList<>();
+        }
+
+        // Complexity: O(1)
+        public void push(Integer x) {
+            q1.offer(x);
+        }
+
+        // Complexity: O(n)
+        public Integer pop() {
+            if (q1.isEmpty()) return null;
+
+            while (q1.size() > 1) {
+                q2.offer(q1.poll());
+            }
+
+            Integer ret = q1.poll();
+
+            // Add all elements back to q1, which is done by assigning
+            q1 = q2;
+            // q2 should be empty again.
+            q2 = new LinkedList<>();
+
+            return ret;
+        }
+
+        // Complexity: O(n)
+        public void push2(Integer x) {
+            q1.offer(x);
+        }
+    }
 }
