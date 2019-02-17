c42957bb813e1908f2f45b5def300c9f1ed98fee
Mahbubur Rahman <mahbub.kuet@gmail.com>
2019-02-17 21:23:24 +0600

    Update Sieve_Demo.java

--- algorithms/java/NumberTheory/src/com/mahbub/algorithm/Sieve_Demo.java
+++ algorithms/java/NumberTheory/src/com/mahbub/algorithm/Sieve_Demo.java
@@ -8,8 +8,8 @@ public class Sieve_Demo {
 
     static final int MAX_PRIME = 10000000;
 
-    static boolean[] flag = new boolean[MAX_PRIME + 7];
-    static List<Integer> primes = new ArrayList<>();
+    static boolean[] flag;
+    static List<Integer> primes;
 
     static class PowerPrime {
         long p;
@@ -17,15 +17,18 @@ public class Sieve_Demo {
         PowerPrime(long p, int a) { this.p = p; this.a = a; };
     }
 
-    static void sieve() {
+    // Generate prime up to N
+    static void generatePrime(int N) {
+        flag = new boolean[N + 7];
+        primes = new ArrayList<>();
         int i = 2;
-        while (i*i <= MAX_PRIME) {
-            for (int j = i*i; j <= MAX_PRIME; j += i) flag[j] = true;
+        while (i * i <= N) {
+            for (int j = i*i; j <= N; j += i) flag[j] = true;
             while (flag[++i]);
         }
         primes.add(2);
-        for (i = 3; i <= MAX_PRIME; i += 2) {
-            if (flag[i] == false) primes.add(i);
+        for (i = 3; i <= N; i += 2) {
+            if (!flag[i]) primes.add(i);
         }
     }
 
@@ -69,8 +72,27 @@ public class Sieve_Demo {
         return ret;
     }
 
+    static int countDivisors(long n) {
+        int ret = 1;
+        int root = (int)Math.sqrt(n);
+        for (int i = 0; i < primes.size() && primes.get(i) <= root; i++) {
+            int p = primes.get(i);
+            if (n % p == 0) {
+                int cnt = 0;
+                while (n % p == 0) {
+                    n /= p;
+                    cnt++;
+                }
+                ret *= (cnt + 1);
+            }
+        }
+        if (n > 1) ret = ret << 1;
+
+        return ret;
+    }
+    
     public static void main(String args[]) {
-        sieve();
+        generatePrime(MAX_PRIME);
         System.out.println("Total Primes: " + primes.size());
         //for (int x : primes) System.out.println(x);
 
