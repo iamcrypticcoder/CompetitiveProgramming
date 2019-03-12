e9bfe8f97a9c00ca977c6192b1d96347d5c71617
Mahbubur Rahman <mahbub.kuet@gmail.com>
2019-03-12 19:47:33 +0600

    Create ETF.cpp

---
+++ Solved Problems/SPOJ/ETF.cpp
@@ -0,0 +1,130 @@
+/*
+  Problem Link: https://www.spoj.com/problems/ETF/
+	Solved By : Kazi Mahbubur Rahman (iamcrypticcoder)
+	Time :
+	Rank :
+	Complexity:
+*/
+
+
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <cctype>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define FOR(i, L, U) for(int i=(int)L; i<=(int)U; i++)
+#define FORD(i, U, L) for(int i=(int)U; i>=(int)L; i--)
+
+#define READ(x) freopen(x, "r", stdin)
+#define WRITE(x) freopen(x, "w", stdout)
+
+#define ff first
+#define ss second
+#define PQ priority_queue
+#define PB push_back
+#define SZ size()
+
+#define EPS 		1e-9
+#define SQR(x) 		((x)*(x))
+#define INF 		2000000000
+#define TO_DEG 		57.29577951
+#define PI 			2*acos(0.0)
+
+#define ALL_BITS					((1 << 31) - 1)
+#define NEG_BITS(mask)				(mask ^= ALL_BITS)
+#define TEST_BIT(mask, i)			(mask & (1 << i))
+#define ON_BIT(mask, i)				(mask |= (1 << i))
+#define OFF_BIT(mask, i)			(mask &= NEG_BITS(1 << i))
+#define IS_POWER_TWO(x)				(x && !(x & (x-1)))
+#define OFF_RIGHTMOST_SET_BIT(x)	(x & (x-1))
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+typedef vector<bool> VB;
+typedef vector<int> VI;
+typedef vector<double> VD;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef map<int, int> MII;
+typedef map<char, int> MCI;
+typedef map<string, int> MSI;
+typedef vector<vector<bool> > VVB;
+typedef vector<vector<int> > VVI;
+typedef vector<vector<double> > VVD;
+typedef vector<vector<PII> > VVPII;
+
+int GCD(int a, int b) { while (b)b ^= a ^= b ^= a %= b;  return a; }
+
+// UP, RIGHT, DOWN, LEFT, UPPER-RIGHT, LOWER-RIGHT, LOWER-LEFT, UPPER-LEFT
+int dx[8] = { -1, 0, 1, 0, -1, 1,  1, -1 };
+int dy[8] = { 0, 1, 0,-1,  1, 1, -1, -1 };
+
+// Represents all moves of a knight in a chessboard
+int dxKnightMove[8] = { -1, -2, -2, -1,  1,  2, 2, 1 };
+int dyKnightMove[8] = {  2,  1, -1, -2, -2, -1, 1, 2 };
+
+inline int src() { int ret; scanf("%d", &ret); return ret; }
+
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
+
+#define MAX 1000000
+
+ULL phi[MAX + 7];
+
+void generatePhi() {
+    phi[1] = 1;
+    for (int i = 2; i <= MAX; i++) {
+        if(!phi[i]) {
+            phi[i] = i-1;
+            for(int j = (i << 1); j <= MAX; j += i) {
+                if(!phi[j]) phi[j] = j;
+                phi[j] *= (i-1);
+                phi[j] /= i;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int i, j, k;
+    int TC, tc;
+    double cl = clock();
+
+    generatePhi();
+
+    //FOR(i, 1, 1000000) cout << i << " " << phi[i] << endl;
+
+    TC = src();
+
+    FOR(tc, 1, TC) {
+        int n = src();
+
+        printf("%llu\n", phi[n]);
+    }
+
+    cl = clock() - cl;
+    fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
+
+    return 0;
+}
