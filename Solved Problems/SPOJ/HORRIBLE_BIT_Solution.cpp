42d1fd765b79a2655cda91919d4daa14eae20537
Mahbubur Rahman <mahbub.kuet@gmail.com>
2019-03-17 00:37:21 +0600

    Create HORRIBLE_BIT_Solution.cpp

---
+++ Solved Problems/SPOJ/HORRIBLE_BIT_Solution.cpp
@@ -0,0 +1,155 @@
+/*
+        Problem Link : https://www.spoj.com/problems/HORRIBLE/
+        Solved By : Kazi Mahbubur Rahman (iamcrypticcoder)
+        Status : AC
+        Time :
+        Rank :
+        Complexity:
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <cctype>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+
+using namespace std;
+
+#define FOR(i, L, U) for(int i=(int)L; i<=(int)U; i++)
+#define FORD(i, U, L) for(int i=(int)U; i>=(int)L; i--)
+
+#define READ(x) freopen(x, "r", stdin)
+#define WRITE(x) freopen(x, "w", stdout)
+
+#define ff first
+#define ss second
+#define PQ priority_queue
+#define PB push_back
+#define SZ size()
+
+#define EPS             1e-9
+#define SQR(x)          ((x)*(x))
+#define INF             2000000000
+#define TO_DEG          57.29577951
+#define PI              2*acos(0.0)
+
+#define ALL_BITS                                ((1 << 31) - 1)
+#define NEG_BITS(mask)                          (mask ^= ALL_BITS)
+#define TEST_BIT(mask, i)                       (mask & (1 << i))
+#define ON_BIT(mask, i)                         (mask |= (1 << i))
+#define OFF_BIT(mask, i)                        (mask &= NEG_BITS(1 << i))
+#define IS_POWER_TWO(x)                         (x && !(x & (x-1)))
+#define OFF_RIGHTMOST_SET_BIT(x)                (x & (x-1))
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> PII;
+typedef pair<uint, uint> PUU;
+typedef pair<double, double> PDD;
+typedef vector<bool> VB;
+typedef vector<int> VI;
+typedef vector<uint> VU;
+typedef vector<double> VD;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef map<int, int> MII;
+typedef map<uint, uint> MUU;
+typedef map<char, int> MCI;
+typedef map<string, int> MSI;
+typedef vector<vector<bool> > VVB;
+typedef vector<vector<int> > VVI;
+typedef vector<vector<double> > VVD;
+typedef vector<vector<PII> > VVPII;
+
+int GCD(int a, int b) { while (b)b ^= a ^= b ^= a %= b;  return a; }
+
+// UP, RIGHT, DOWN, LEFT, UPPER-RIGHT, LOWER-RIGHT, LOWER-LEFT, UPPER-LEFT
+int dx[8] = { -1, 0, 1, 0, -1, 1,  1, -1 };
+int dy[8] = { 0, 1, 0,-1,  1, 1, -1, -1 };
+
+// Represents all moves of a knight in a chessboard
+int dxKnightMove[8] = { -1, -2, -2, -1,  1,  2, 2, 1 };
+int dyKnightMove[8] = {  2,  1, -1, -2, -2, -1, 1, 2 };
+
+inline int srcInt() { int ret; scanf("%d", &ret); return ret; }
+inline int srcUInt() { uint ret; scanf("%u", &ret); return ret; }
+inline int srcLongLong() { long long ret; scanf("%lld", &ret); return ret; }
+inline int srcULongLong() { unsigned long long ret; scanf("%llu", &ret); return ret; }
+
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
+
+#define MAX 100000
+
+uint N, C;
+LL bit1[MAX + 7];
+LL bit2[MAX + 7];
+
+void update(LL bit[], int idx, LL val) {
+    while(idx <= N) {
+        bit[idx] += val;
+        idx += (idx & -idx);
+    }
+}
+
+LL read(LL bit[], LL idx) {
+    LL sum = 0;
+    while (idx > 0) {
+        sum += bit[idx];
+        idx -= (idx & -idx);
+    }
+    return sum;
+}
+
+int main()
+{
+    READ("input.txt");
+    //WRITE("output.txt");
+    int i, j, k;
+    uint TC, tc;
+    double cl = clock();
+
+    TC = srcUInt();
+    FOR(tc, 1, TC) {
+        N = srcUInt();
+        C = srcUInt();
+        memset(bit1, 0, sizeof bit1);
+        memset(bit2, 0, sizeof bit2);
+        FOR(c, 1, C) {
+            uint cmd = srcUInt();
+            int p = srcUInt();
+            int q = srcUInt();
+            if (cmd == 0) {
+                int v = srcUInt();
+                update(bit1, p, v);
+                update(bit1, q+1, -v);
+                update(bit2, p, -v * (LL)(p-1));
+                update(bit2, q+1, v * (LL)q);
+            } else {
+                LL ans = read(bit1, q) * q + read(bit2, q) - read(bit1, p-1) * (p-1) - read(bit2, p-1);
+                printf("%lld\n", ans);
+            }
+        }
+    }
+
+    cl = clock() - cl;
+    fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
+
+    return 0;
+}
