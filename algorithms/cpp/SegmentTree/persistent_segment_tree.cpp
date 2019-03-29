40aecf4a5c3eababe771ea082439fe0b84d8faac
Mahbubur Rahman <mahbub.kuet@gmail.com>
2019-03-29 12:52:23 +0600

    Update persistent_segment_tree.cpp

--- algorithms/cpp/SegmentTree/persistent_segment_tree.cpp
+++ algorithms/cpp/SegmentTree/persistent_segment_tree.cpp
@@ -1,9 +1,10 @@
 /*
-    Problem Link: https://uva.onlinejudge.org/external/125/12532.pdf
-	Solved By : Kazi Mahbubur Rahman (iamcrypticcoder)
-	Time : 0.150
-	Rank : 891
-	Complexity:
+        Problem Link : https://www.spoj.com/problems/PSEGTREE/
+        Solved By : Kazi Mahbubur Rahman (iamcrypticcoder)
+        Status : AC
+        Time : 0.31
+        Rank :
+        Complexity:
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -24,6 +25,7 @@
 #include <cstring>
 #include <sstream>
 #include <iostream>
+#include <limits.h>
 #include <algorithm>
 
 using namespace std;
@@ -40,30 +42,33 @@ using namespace std;
 #define PB push_back
 #define SZ size()
 
-#define EPS 		1e-9
-#define SQR(x) 		((x)*(x))
-#define INF 		2000000000
-#define TO_DEG 		57.29577951
-#define PI 			2*acos(0.0)
+#define EPS             1e-9
+#define SQR(x)          ((x)*(x))
+#define INF             2000000000
+#define TO_DEG          57.29577951
+#define PI              2*acos(0.0)
 
-#define ALL_BITS					((1 << 31) - 1)
-#define NEG_BITS(mask)				(mask ^= ALL_BITS)
-#define TEST_BIT(mask, i)			(mask & (1 << i))
-#define ON_BIT(mask, i)				(mask |= (1 << i))
-#define OFF_BIT(mask, i)			(mask &= NEG_BITS(1 << i))
-#define IS_POWER_TWO(x)				(x && !(x & (x-1)))
-#define OFF_RIGHTMOST_SET_BIT(x)	(x & (x-1))
+#define ALL_BITS                                ((1 << 31) - 1)
+#define NEG_BITS(mask)                          (mask ^= ALL_BITS)
+#define TEST_BIT(mask, i)                       (mask & (1 << i))
+#define ON_BIT(mask, i)                         (mask |= (1 << i))
+#define OFF_BIT(mask, i)                        (mask &= NEG_BITS(1 << i))
+#define IS_POWER_TWO(x)                         (x && !(x & (x-1)))
+#define OFF_RIGHTMOST_SET_BIT(x)                (x & (x-1))
 
 typedef long long LL;
 typedef unsigned long long ULL;
 typedef pair<int, int> PII;
+typedef pair<uint, uint> PUU;
 typedef pair<double, double> PDD;
 typedef vector<bool> VB;
 typedef vector<int> VI;
+typedef vector<uint> VU;
 typedef vector<double> VD;
 typedef vector<char> VC;
 typedef vector<string> VS;
 typedef map<int, int> MII;
+typedef map<uint, uint> MUU;
 typedef map<char, int> MCI;
 typedef map<string, int> MSI;
 typedef vector<vector<bool> > VVB;
@@ -79,81 +84,105 @@ int dy[8] = { 0, 1, 0,-1,  1, 1, -1, -1 };
 
 // Represents all moves of a knight in a chessboard
 int dxKnightMove[8] = { -1, -2, -2, -1,  1,  2, 2, 1 };
-int dyKnightMove[8] = { 2,  1, -1, -2, -2, -1, 1, 2 };
+int dyKnightMove[8] = {  2,  1, -1, -2, -2, -1, 1, 2 };
 
-inline int src() { int ret; scanf("%d", &ret); return ret; }
+inline int srcInt() { int ret; scanf("%d", &ret); return ret; }
+inline int srcUInt() { uint ret; scanf("%u", &ret); return ret; }
+inline int srcLongLong() { long long ret; scanf("%lld", &ret); return ret; }
+inline int srcULongLong() { unsigned long long ret; scanf("%llu", &ret); return ret; }
 
 #define WHITE 0
 #define GRAY 1
 #define BLACK 2
 
-#define MAX 100000
+#define MAX_N 100000
 
-inline int left(int p) { return p << 1; }
-inline int right(int p) { return (p << 1) + 1; }
-
-// Node for Segment Tree RSQ
 struct Node {
-    int val;
-    int ln, rn;
-    Node() {
-        ln = rn = -1;
+    Node *left, *right;
+    int sum = 0;
+
+    Node(int val) {
+        this->left = this->right = nullptr;
+        this->sum = val;
     }
-    void combine(Node &a, Node &b) {
-        val = a.val + b.val;
+    Node(Node *l, Node *r) {
+        this->left = l;
+        this->right = r;
+        if (l) this->sum += l->sum;
+        if (r) this->sum += r->sum;
     }
 };
 
-int N, Q;
-int A[MAX + 7];
-Node st[8 * MAX + 7];       // Segment Tree node pool
-int nodeNum = -1;
+uint N, Q;
+int A[MAX_N];
+//Node* rootMap[MAX_N];
+map<int, Node*> rootMap;
 
-int newLeaf(int val) {
-    Node node = st[++nodeNum];
-    node.val = val;
-    return nodeNum;
-}
-
-int newParent(int ln, int rn) {
-    Node node = st[++nodeNum];
-    node.ln = ln;
-    node.rn = rn;
-    node.combine(st[ln], st[rn]);
-    return nodeNum;
-}
-
-int build(int l, int r) {
-    if (l == r) return newLeaf(A[l]);
-    int mid = (l + r) >> 1;
-    return newParent(build(l, mid), build(mid+1, r));
-}
-
-int updateSingle(int p, int l, int r, int i, int val) {
-    if (l == r) return newLeaf(st[p].val + val);
-    int mid = (l + r) >> 1;
-    if (i <= mid) {
-        int ln = updateSingle(st[p].ln, l, mid, i, val);
-        return newParent(ln, st[p].rn);
-    } else {
-        int rn = updateSingle(st[p].rn, mid+1, rn, i, val);
-        return newParent(st[p].ln, rn);
+Node* build(int l, int r) {
+    if (l == r) {
+        return new Node(A[l]);
     }
+    int mid = (l + r) >> 1;
+    Node* left = build(l, mid);
+    Node* right = build(mid+1, r);
+    return new Node(left, right);
+}
+
+int querySum(Node* n, int l, int r, int i, int j) {
+    if (i == l && j == r) {
+        return n->sum;
+    }
+
+    int mid = (l + r) >> 1;
+    if (j <= mid) return querySum(n->left, l, mid, i, j);
+    if (i > mid) return querySum(n->right, mid+1, r, i, j);
+
+    int tmp1 = querySum(n->left, l, mid, i, mid);
+    int tmp2 = querySum(n->right, mid+1, r, mid+1, j);
+    return tmp1 + tmp2;
+}
+
+Node* update(Node* n, int l, int r, int i, int v) {
+    if (l == r) {
+        return new Node(n->sum + v);
+    }
+    int mid = (l + r) >> 1;
+    if (i <= mid)
+        return new Node(update(n->left, l, mid, i, v), n->right);
+    else
+        return new Node(n->left, update(n->right, mid+1, r, i, v));
 }
 
 int main()
 {
-    //READ("input.txt");
-    //WRITE("output.txt");
     int i, j, k;
-    int TC, tc;
+    uint TC, tc;
     double cl = clock();
 
-    int arr[7] = { 8, 7, 3, 9, 5, 1, 10 };
-    N = 7;
-    FOR(i, 0, N - 1) A[i] = arr[i];
+    int versionIndex = 0;
 
-    
+    N = srcUInt();
+    FOR(i, 0, N-1) A[i] = srcInt();
+    rootMap[versionIndex++] = build(0, N-1);
+
+    Q = srcUInt();
+    FOR(i, 0, Q-1) {
+        int cmd = srcInt();
+        int idx = srcInt();
+        if (cmd == 1) {
+            int pos = srcInt() - 1;
+            int v = srcInt();
+            Node* root = rootMap[idx];
+            rootMap[versionIndex++] = update(root, 0, N-1, pos, v);
+        } else {
+            int l = srcInt() - 1;
+            int r = srcInt() - 1;
+            Node* root = rootMap[idx];
+            int ans = querySum(root, 0, N-1, l, r);
+            printf("%d\n", ans);
+        }
+    }
+
 
     cl = clock() - cl;
     fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
