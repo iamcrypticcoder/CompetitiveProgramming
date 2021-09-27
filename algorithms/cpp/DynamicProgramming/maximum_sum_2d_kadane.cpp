77f0ff720f919d763de66202616912109dd60c4a
Mahbubur Rahman <mahbub.kuet@gmail.com>
2021-09-28 02:37:59 +0600

    Update maximum_sum_2d_kadane.cpp

--- algorithms/cpp/DynamicProgramming/maximum_sum_2d_kadane.cpp
+++ algorithms/cpp/DynamicProgramming/maximum_sum_2d_kadane.cpp
@@ -28,70 +28,40 @@
 
 using namespace std;
 
-#define FOR(i, L, U) for(int i=(int)L; i<=(int)U; i++)
-#define FORD(i, U, L) for(int i=(int)U; i>=(int)L; i--)
-
 #define READ(x) freopen(x, "r", stdin)
 #define WRITE(x) freopen(x, "w", stdout)
 
-#define PQ priority_queue
-#define PB push_back
-#define SZ size()
+int maximumSum2D(vector<vector<int>> v) {
+    if (v.size() == 0 || v[0].size() == 0) return 0;
 
-#define EPS 1e-9
-#define SQR(x) ((x)*(x))
-#define INF 99999999
-#define TO_DEG 57.29577951
-#define PI 2*acos(0.0)
+    int m = v.size();
+    int n = v[0].size();
 
-#define ALL_BITS ((1 << 31) - 1)
-#define NEG_BITS(mask) (mask ^= ALL_BITS)
-#define TEST_BIT(mask, i) (mask & (1 << i))
-#define ON_BIT(mask, i) (mask |= (1 << i))
-#define OFF_BIT(mask, i) (mask &= NEG_BITS(1 << i))
+    // Start from 2nd row
+    for (int i = 1; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            v[i][j] = v[i-1][j] + v[i][j];
+        }
+    }
 
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef vector<int> VI;
-typedef vector<vector<int> > VVI;
-typedef vector<string> VS;
-typedef vector<bool> VB;
-typedef vector<char> VC;
-typedef vector< vector<bool> > VVB;
-typedef pair<int, int> PII;
-typedef map<int, int> MII;
-typedef map<char, int> MCI;
-typedef map<string, int> MSI;
+    int maxSum = v[0][0];
+    for (int i = 0; i < m; i++) {
+        for (int j = i; j < m; j++) {
+            int curMax = (i == 0 ? v[j][0] : v[j][0] - v[i-1][0]);
+            for (int k = 1; k < n; k++) {
+                int val = i == 0 ? v[j][k] : v[j][k] - v[i-1][k];
+                curMax = max(val, curMax + val);
+                maxSum = max(maxSum, curMax);
+            }
+        }
+    }
 
-int GCD(int a,int b)    {   while(b)b^=a^=b^=a%=b;  return a;   }
-int LCM(int a, int b)   {   return a/GCD(a,b)*b;                }
+    return maxSum;
+}
 
-// UP, RIGHT, DOWN, LEFT, UPPER-RIGHT, LOWER-RIGHT, LOWER-LEFT, UPPER-LEFT
-int dx[8] = {-1, 0, 1, 0, -1, 1,  1, -1};
-int dy[8] = { 0, 1, 0,-1,  1, 1, -1, -1};
-
-// Represents all moves of a knight in a chessboard
-int dxKnightMove[8] = {-1, -2, -2, -1,  1,  2, 2, 1};
-int dyKnightMove[8] = { 2,  1, -1, -2, -2, -1, 1, 2};
-
-inline int src() { int ret; scanf("%d", &ret); return ret; }
-
-#define WHITE 0
-#define GRAY 1
-#define BLACK 2
-
-#define MAX 10000
-
-int R, C;
-VVI A;
-int maxSum;
-
-/**
-This solution handles when all values are negative.
-**/
 int main()
 {
-    READ("input.txt");
+    //READ("../input.txt");
     //WRITE("output.txt");
     int i, j, k;
     int TC, tc;
@@ -99,25 +69,13 @@ int main()
     double cl = clock();
     cl = clock() - cl;
 
-    while(cin >> R >> C) {
-        A = VVI(R+1, VI(C+1));
+    int m, n;
+    while (cin >> m >> n) {
+        vector<vector<int>> v = vector<vector<int>>(m, vector<int>(n));
+        for (int i = 0; i < m; i++) for (int j = 0; j < n; j++)
+            cin >> v[i][j];
 
-        FOR(i, 1, R) FOR(j, 1, C) {
-            cin >> A[i][j];
-            A[i][j] += A[i-1][j];
-        }
-
-        maxSum = A[1][1];
-        FOR(i, 1, R) FOR(j, i, R) {
-            int curMax = (A[j][1] - A[i-1][1]);
-            FOR(k, 2, C) {
-                int val = A[j][k] - A[i-1][k];
-                curMax = max(val, curMax + val);
-                maxSum = max(maxSum, curMax);
-            }
-        }
-
-        cout << maxSum << endl;
+        cout << maximumSum2D(v) << endl;
     }
 
     fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
