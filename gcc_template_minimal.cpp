ffa34fdbdcb5fc2c871ab9a9a448dd130b9b89b9
Mahbubur Rahman <mahbub.kuet@gmail.com>
2022-04-20 22:40:26 +0600

    Create gcc_template_minimal.cpp

---
+++ gcc_template_minimal.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define READ(x) freopen(x, "r", stdin)
+#define WRITE(x) freopen(x, "w", stdout)
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //READ("../input.txt");
+    //WRITE("output.txt");
+
+    int64_t TC, tc;
+    double cl = clock();
+
+    for (tc = 1; tc <= TC; tc++) {
+        // Start code for a test case
+    }
+
+    cl = clock() - cl;
+    fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
+
+    return 0;
+}
